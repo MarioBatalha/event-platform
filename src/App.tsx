f87452242de7966f59da81320dcd6ac0209cd4c6
MarioBatalha <mariobatalha9@gmail.com>
2022-06-22 15:43:52 +0100

    feat: handle data with apollo client

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { gql, useQuery } from '@apollo/client';
+import { client } from './lib/apollo';
 
-import './styles/global.css';
-
+const GET_LESSON_QUERY = gql`
+query {
+  lessons {
+    id
+    title
+  }
+}`
 const App = () => {
+  const { data } = useQuery(GET_LESSON_QUERY);
+  console.log(data)
   return (
     <h1 className="text-2xl">
       Hi there
